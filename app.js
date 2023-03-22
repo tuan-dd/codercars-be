6938c58aa38d2f69635c38a01825028fd90b9055
tuan-dd <tuandd.310797@gmail.com>
2023-03-22 15:09:11 +0700

    adujst url in cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const mongoose = require('mongoose');
 const allowlist = [
    'http://localhost:5500/',
    'http://localhost:3000',
-   'https://codercars-be-production-3f75.up.railway.app/',
+   'https://codercars-be-production-3f75.up.railway.app',
 ];
 app.use(cors({ origin: allowlist }));
 
